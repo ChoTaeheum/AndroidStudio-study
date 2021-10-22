80cdb31e2861c39ff263e4feda579f786dc644f9
ChoTaeheum <taeheumcho89@gmail.com>
2021-10-22 14:51:16 +0900

    10/22 update3

--- KotlinSample/app/src/main/java/com/example/kotlinsample/ControlKotlinActivity.kt
+++ KotlinSample/app/src/main/java/com/example/kotlinsample/ControlKotlinActivity.kt
@@ -6,6 +6,10 @@ import android.os.Bundle
 import android.widget.Toast
 import kotlinx.android.synthetic.main.layout_control.*
 
+
+
+
+
 class ControlKotlinActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,14 +21,15 @@ class ControlKotlinActivity : AppCompatActivity() {
             // numberField의 값을 읽어 int 형으로 변환한다.
             val number = numberField.text.toString().toInt()
 
-            // if, else if, else 문으로 2의 배수, 3의 배수를 체크해 서로 다른 토스트 메세지를 보여준다.
-            if (number % 2 == 0) {
-                Toast.makeText(applicationContext, "${number} 는 2의 배수입니다.", Toast.LENGTH_SHORT).show() }
-            else if (number % 3 == 0) {
-                Toast.makeText(applicationContext, "${number} 는 3의 배수입니다.", Toast.LENGTH_SHORT).show() }
-            else {
-                Toast.makeText(applicationContext, "${number}", Toast.LENGTH_SHORT).show() }
-
+            // when 문은 케이스로 조건식도 사용가능
+            when {
+                number % 2 == 0 ->
+                    Toast.makeText(applicationContext, "${number} 는 2의 배수입니다.", Toast.LENGTH_SHORT).show()
+                number % 3 == 0 ->
+                    Toast.makeText(applicationContext, "${number} 는 3의 배수입니다.", Toast.LENGTH_SHORT).show()
+                else ->
+                    Toast.makeText(applicationContext, "${number}", Toast.LENGTH_SHORT).show()
+            }
             // 코틀린에서는 switch 문을 대체해 when 을 사용할 수 있다.
             when (number) {
                 // case 1~4까지 같은 코드 실행
